d47926f9682c3873cc01a957cdd06f2ea8de7e13
Vinayaka Vagga <vinayakavagga@outlook.com>
2026-08-01 12:43:58 +0530

    Enabled Swagger in prod

--- Program.cs
+++ Program.cs
@@ -24,11 +24,14 @@ builder.Services.AddScoped<IImageBlogRepository, ImageRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
